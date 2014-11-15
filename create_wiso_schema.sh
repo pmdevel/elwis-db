f7478970523e051dd1af47a60cb48cad73851caa
nikahl <niclas.ahlstrand@pensionsmyndigheten.se>
2014-11-15 18:11:49 +0100

    Fixat felaktig kopiering

--- create_wiso_schema.sh
+++ create_wiso_schema.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-su -l -c "cp /tmp/docker_install_dir/create_wiso_schema.sql /u01/app/oracle/ " oracle
+su -l -c "cp /tmp/docker_install_dir/WISO.sql /u01/app/oracle/ " oracle
 su -l -c "source $ORACLE_HOME/bin/oracle_env.sh; sqlplus / as sysdba @WISO" oracle
